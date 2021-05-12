b33d4dacac5f0178f85943d4e7e396ea33e552cb
BeanTosser <56370925+BeanTosser@users.noreply.github.com>
2021-05-11 20:58:50 -0400

    Work on converting state initialization.

--- src/Tools/classToFunctionalConverter.js
+++ src/Tools/classToFunctionalConverter.js
@@ -1,48 +1,18 @@
 export default function (componentString) {
-  /*
-   * Define regex patterns
-   *
-   * - class def
-   * - constructor (essentially remove constructor container declaration
-   *   and "super(props)" IF PRESENT)
-   * - state initialization
-   * - convert "this.setState({var1: value1...varn: valuen});"
-   *   with "setVar1(value1);...setVarn(valuen);"
-   * - find all lifecycle methods:
-   *   -componentDidMount
-   *     -useEffect(function(){action}, []);
-   *   -componentDidUpdate
-   *     -useEffect(function(){action});
-   *   -componentDidUpdate (with variable change)
-   *     (ie prevprops.prop != props.prop)
-   *     -useEffect(function(){action}, [props.prop]);
-   *   -componentWillUnmount
-   *     -useEffect(function(){return(action)},[]);
-   * - Replace instances of "this.state.var" with "var"
-   * - remove final "}" (from class def)
-   * - export default:
-   *   - if it appears at end of file as "export default classname",
-   *     remove that line entirely
-   *   - otherwise simple leave it as is before the converted class
-   *     to functional declaration
-   */
-
-  // Position trackers - remember where state was set etc. in order to change programatically later
-  let initializeStatePosition, endIntializeStatePosition;
-
   // REGEX PATTERNS
   const regexPatterns = {
     classDeclarationRegex: /class ((\w*|\d*)+) extends React.Component *{ *\n/gim,
     // blah? remove outer constructor definition block and fix contents indentation accordingly
     constructorRegex: /( *constructor\(props\) *{ *\n)(?: *super\(props\);? *\n)(((?: {2}).*\n)*)/gim,
-    initializeStateRegex: /(?: *this.state ?= ?{ *\n)((.+\n+)+?(?= *}))(?: *};? *\n)/gim
+    initializeStateRegex: /(?: *this.state ?= ?{ *\n)((.+\n+)+?(?= *}))(?: *};? *\n)/gim,
+    setStateVarRegex: / *(\S*)?(?=:): (\S*),? *\n?/y
   };
   const replacements = {
     classDeclarationReplacement: "function $1(props) {\n",
     constructorReplacement: "$2",
     initializeStateReplacement: "$1"
   };
-
+  /*
   componentString = componentString.replace(
     regexPatterns.classDeclarationRegex,
     replacements.classDeclarationReplacement
@@ -51,10 +21,40 @@ export default function (componentString) {
     regexPatterns.constructorRegex,
     replacements.constructorReplacement
   );
+*/
+  console.log("Searching for state initialization...");
+  // Find position in the code where state is initialized (denoted by "this.state =")
+  let initializeStatePosition = componentString.indexOf(
+    regexPatterns.initializeStateRegex
+  );
+  console.log(
+    "state is initialized at string position: " + initializeStatePosition
+  );
+  // If state initialization exists
+  if (initializeStatePosition >= 0) {
+    // Create a copy of the string omitting everything _before_ state initialization
+    // just in case the code sets any other object values before the state initialization
+    let codeFromStateInitialization = componentString.slice(
+      initializeStatePosition,
+      componentString.length
+    );
+    while (regexPatterns.setStateVarRegex.exec(codeFromStateInitialization)) {
+      codeFromStateInitialization = codeFromStateInitialization.replace(
+        regexPatterns.setStateVarRegex,
+        ""
+      );
+    }
+    // COncat everything before the state initializatin position and the newly modified code
+    // and put it back into componentString
+    componentString =
+      componentString.slice(0, initializeStatePosition) +
+      codeFromStateInitialization;
+  }
+  /*
   componentString = componentString.replace(
     regexPatterns.initializeStateRegex,
     replacements.initializeStateReplacement
   );
-
+*/
   return componentString;
 }
