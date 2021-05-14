b175bb11f6a5315a64262980f9c8e5bc371967f2
BeanTosser <56370925+BeanTosser@users.noreply.github.com>
2021-05-13 20:31:58 -0400

    Continue working on replacing state initialization

--- src/Tools/classToFunctionalConverter.js
+++ src/Tools/classToFunctionalConverter.js
@@ -5,15 +5,15 @@ export default function (componentString) {
     // blah? remove outer constructor definition block and fix contents indentation accordingly
     constructorRegex: /( *constructor\(props\) *{ *\n)(?: *super\(props\);? *\n)(((?: {2}).*\n)*)/gim,
     initializeStateRegex: /(?: *this.state ?= ?{ *\n)((.+\n+)+?(?= *}))(?: *};? *\n)/gim,
-    setStateVarRegex: /( *)((\S)(\S*))(?=:): ((\S)(\S*))(?=,)?,? *\n?/g,
-    useStateSetter: /(set([a-z]))(?<=(const \[\2([a-zA-Z0-9_]*)\]) = useState))/g
+    setStateVarRegex: /([a-z]\w*): ([^\s,]*)/g,
+    useStateSetter: /set([a-z])\w*/g
   };
   const replacements = {
     classDeclarationReplacement: "function $1(props) {\n",
     constructorReplacement: "$2",
-    initializeStateReplacement: "$1const [$2$3, set$2$3] = useState($3);\n"
+    initializeStateReplacement: "const [$1, set$1] = useState($2);\n"
   };
-  /*
+
   componentString = componentString.replace(
     regexPatterns.classDeclarationRegex,
     replacements.classDeclarationReplacement
@@ -22,54 +22,91 @@ export default function (componentString) {
     regexPatterns.constructorRegex,
     replacements.constructorReplacement
   );
-*/
-  console.log("Searching for state initialization...");
+
   // Find position in the code where state is initialized (denoted by "this.state =")
-  let initializeStatePosition = componentString.search(
+  let initializeStateRegexMatch = componentString.match(
     regexPatterns.initializeStateRegex
   );
-  console.log(
-    "state is initialized at string position: " + initializeStatePosition
-  );
+  let initializeStateRange = [
+    initializeStateRegexMatch.index,
+    initializeStateRegexMatch.index + initializeStateRegexMatch.length
+  ];
+
   // If state initialization exists
-  if (initializeStatePosition >= 0) {
+  if (initializeStateRange[0] >= 0) {
     console.log("replacing state initialization...");
     // Create a copy of the string omitting everything _before_ state initialization
     // just in case the code sets any other object values before the state initialization
-    let codeFromStateInitialization = componentString.slice(
-      initializeStatePosition,
-      componentString.length
+    let stateInitializationCode = componentString.slice(
+      initializeStateRange[0],
+      initializeStateRange[1]
     );
-    let match = regexPatterns.setStateVarRegex.exec(
-      codeFromStateInitialization
-    );
+    let match = regexPatterns.setStateVarRegex.exec(stateInitializationCode);
     console.log("Match: " + match);
     for (let i = 0; i < match.length; i++) {
-      codeFromStateInitialization = codeFromStateInitialization.replace(
+      stateInitializationCode = stateInitializationCode.replace(
         regexPatterns.setStateVarRegex,
         replacements.initializeStateReplacement
       );
     }
+    console.log(
+      "code after replacing state var decs: " + stateInitializationCode
+    );
     // The first letter of the var name should be Uppercase in the
     // setter function name for proper camel casing
-    match = regexPatterns.useStateSetter.exec(codeFromStateInitialization);
-    for (let i = 0; i < match.length; i++) {
-      let matchIndex = match[i].index;
-      let character = codeFromStateInitialization.charAt(matchIndex);
-      character = character.toUpper();
-      codeFromStateInitialization =
-        codeFromStateInitialization.subString(0, matchIndex) +
+    let useStatePosition = stateInitializationCode.search(
+      regexPatterns.useStateSetter
+    );
+    let character;
+    let counter = 0;
+    while (useStatePosition >= 0) {
+      character = stateInitializationCode.charAt(useStatePosition + 3);
+      character = character.toUpperCase();
+      stateInitializationCode =
+        stateInitializationCode.substring(0, useStatePosition + 3) +
         character +
-        codeFromStateInitialization.subString(
+        stateInitializationCode.substring(
+          useStatePosition + 4,
+          stateInitializationCode.length
+        );
+      counter++;
+      if (counter > 500) break; // Safety feature to prevent infinite looping in case of unusually-formatted code
+      useStatePosition = stateInitializationCode.search(
+        regexPatterns.useStateSetter
+      );
+    }
+
+    /*
+    match = regexPatterns.useStateSetter.exec(stateInitializationCode);
+    for (let i = 0; i < match.length; i++) {
+      console.log("match[i]: " + match[i]);
+      let matchIndex = match[i].index;
+      console.log("matchIndex: " + matchIndex);
+      // matchIndex is the index of the word "set" in "setvarName".
+      // In this case, we need to replace the lowercase "v" with a "V",
+      // which appears 3 characters after the match position.
+      let character = stateInitializationCode.charAt(matchIndex);
+      console.log("Char: " + character);
+      character = character.toUpperCase();
+      console.log("Upper char: " + character);
+      stateInitializationCode =
+        stateInitializationCode.substring(0, matchIndex) +
+        character +
+        stateInitializationCode.substring(
           matchIndex,
-          codeFromStateInitialization.length
+          stateInitializationCode.length
         );
     }
+    */
     // COncat everything before the state initializatin position and the newly modified code
     // and put it back into componentString
     componentString =
-      componentString.slice(0, initializeStatePosition) +
-      codeFromStateInitialization;
+      componentString.slice(0, initializeStateRange[0]) +
+      stateInitializationCode +
+      componentString.slice(
+        initializeStateRange[1] + 1,
+        stateInitializationCode.length
+      );
   }
   /*
   componentString = componentString.replace(
